5448e10b80987e1608dc037c7089b63cf1458fbb
Steven Zimmer <webdevzim@gmail.com>
2024-02-14 08:16:26 -0800

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.liquid',
+    './frontend/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1180px",
+        },
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        fraunces: ["Fraunces", "serif"],
+      },
+      colors: {
+        bp: {
+          primary: "#2E2A39",
+          body: "#625F6B",
+          lavender: "#DABDFF",
+          green: "#4AA471",
+          orange: "#F07961",
+          yelloy: "#FFD174",
+          "blue-light": "#B8DDE5",
+          "blue-dark": "#152430",
+          "teal-light": "#5FB9B3",
+          "teal-dark": "#016366",
+          "grey-1": "#F9F9F7",
+          "grey-2": "#F3F3EF",
+          "grey-3": "#EEECE7",
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
+}
